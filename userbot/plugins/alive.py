d3270cc854508c42ac71a36a3f198361025aaa9e
SkyRimZx <57332128+SkyRimZx@users.noreply.github.com>
2020-06-13 14:56:36 +0700

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -13,19 +13,19 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**MY STATUS** \n`FRIDAY IS:` **✅ Alive**\n\n"
+    await alive.edit("**MY STATUS** \n`S.K.Y IS:` **✅ Alive**\n\n"
                      "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
-                     "`DATABASE STATUS:` **ALL NORMAL! WORKING FINE 🙂**\n`NO PROBLEM FOUND🔥!\n`"
+                     "`DATABASE STATUS:` **ALL NORMAL! WORKING FINE **\n`NO PROBLEM FOUND🔥!\n`"
                      "`CONNECTION PROVIDER:` ** AMAZON WEB SERVICES\n"
-                     "`CURRECT BOT LOCATION:` ** ASHBURN,UNITED STATES\n"
+                     "`CURRECT BOT LOCATION:` ** JAKARTA, INDONESIA\n"
                      "`DEPLOYING SERVICE BY: HEROKU INC.\n"
                      "`IP:` 3.82.66.232\n"
                      "`SYSTEM TYPE:` LINUX\n"
-                     "`BASED ON:` UNIBORG\n"
-                     "`SATELLITE:` ⚡STARK SAT-2⚡\n"
+                     "`BASED ON:` SKYBOT\n"
+                     "`SATELLITE:` SKY SAT-2\n"
                      "`MADE USING:` PYTHON\n"
                      f"`MY BOSS`: {DEFAULTUSER}\n\n"
                      "ALWAYS WITH YOU MY BOSS\n\n"
                      "`SATELLITE STATUS: ✅ Alive\n\n"
-                     "[⚡DEPLOY FRIDAY⚡](https://github.com/StarkGang/FRIDAYUserbot)") 
+
 
